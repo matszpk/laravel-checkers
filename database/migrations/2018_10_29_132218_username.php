c1040c4a48f8ed3f54b7f645863395ab9ae76d3e
matszpk <matszpk@interia.pl>
2018-10-29 15:21:34 +0100

    Add missing migration.

---
+++ database/migrations/2018_10_29_132218_username.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Username extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function(Blueprint $table) {
+            $table->unique('name')->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropUnique('name')->dropIndex('name');
+        });
+    }
+}
