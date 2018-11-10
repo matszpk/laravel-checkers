b6e0fa5e22ab0f78e2ff2f7874385175b6130652
matszpk <matszpk@interia.pl>
2018-11-10 16:52:53 +0100

    Remove obsolete use in GameController. Embrace makeMove code by DB transaction.

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,7 +8,7 @@ use App\Logic\GameLogic;
 use App\Logic\GameException;
 use App\Move;
 use Illuminate\Http\Request;
-use App\Logic\GameLogic;
+use Illuminate\Support\Facades\DB;
 
 class GameController extends Controller
 {
@@ -29,75 +29,87 @@ class GameController extends Controller
 
     private const GameResultNames = [NULL, 'player1', 'player2', 'draw'];
 
-    public function makeMove(Request $request, int $gameId, int $startPos, int $endPos)
+    public function makeMove(int $gameId, int $startPos, int $endPos)
     {
-        $game = Game::find($gameId);
+        $error = NULL;
+        $outIsPlayer1Move = False;
 
-        $currentTime = now();
-        // authorizatrion
-        $this->authorize('play', $game);
-        $lastBeat = NULL;
-        if ($game->last_beat !== NULL)
-            $lastBeat = [$game->last_start, $game->last_beat];
+        DB::transaction(function() use ($gameId, $startPos, $endPos, &$error,
+                &$outIsPlayer1Move) {
+            $game = Game::find($gameId);
 
-        $board = [];
-        for ($i = 0; $i < GameLogic::BOARDDIM*GameLogic::BOARDDIM; $i++)
-            $board[$i] = $game->board[$i];
+            $currentTime = now();
+            // authorizatrion
+            $this->authorize('play', $game);
+            $lastBeat = NULL;
+            if ($game->last_beat !== NULL)
+                $lastBeat = [$game->last_start, $game->last_beat];
 
-        // initialize Game logic
-        $gameLogic = GameLogic::fromData($game->board, $game->player1_move,
-                $last_beat);
-        $doneByPlayer1 = $gameLogic->isPlayer1MakeMove();
+            $board = [];
+            for ($i = 0; $i < GameLogic::BOARDDIM*GameLogic::BOARDDIM; $i++)
+                $board[$i] = $game->board[$i];
 
-        if ($gameLogic->checkGameEnd() != GameLogic::NOTEND)
-            // if end of game
-            return [ 'error' => 'End of game' ];
+            // initialize Game logic
+            $gameLogic = GameLogic::fromData($game->board, $game->player1_move,
+                    $last_beat);
+            $doneByPlayer1 = $gameLogic->isPlayer1MakeMove();
 
-        // try to make move
-        try
-        {
-            $gameLogic->makeMove($startPos, $endPos);
-        }
-        catch (GameException $ex)
-        {
-            return [ 'error' => $ex->getMessage() ];
-        }
-        // save move in database
-        $move = new Move([ 'startpos' => $startPos, 'endPos' => $endPos,
-                'done_at' => $currentTime, 'done_by_player1' => $doneByPlayer1 ]);
-        $lastBeat = $gameLogic->getLastBeat();
+            if ($gameLogic->checkGameEnd() != GameLogic::NOTEND)
+            {
+                // if end of game
+                $error = 'error' => $ex->getMessage();
+                return;
+            }
 
-        // update board
-        $outBoard = '';
-        for ($i = 0; $i < GameLogic::BOARDDIM*GameLogic::BOARDDIM; $i++)
-            $outBoard .= $gameLogic->getBoard()[$i];
+            // try to make move
+            try
+            {
+                $gameLogic->makeMove($startPos, $endPos);
+            }
+            catch (GameException $ex)
+            {
+                $error = 'error' => $ex->getMessage();
+                return;
+            }
+            // save move in database
+            $move = new Move([ 'startpos' => $startPos, 'endPos' => $endPos,
+                    'done_at' => $currentTime, 'done_by_player1' => $doneByPlayer1 ]);
+            $lastBeat = $gameLogic->getLastBeat();
 
-        // update last beat
-        if ($lastBeat !== NULL)
-        {
-            $game->last_start = $lastBeat[0];
-            $game->last_beat = $lastBeat[1];
-        }
-        else
-        {
-            // if null
-            $game->last_start = NULL;
-            $game->last_beat = NULL;
-        }
-        // update current isplayer1move
-        $game->player1_move = $gameLogic->isPlayer1MakeMove();
+            // update board
+            $outBoard = '';
+            for ($i = 0; $i < GameLogic::BOARDDIM*GameLogic::BOARDDIM; $i++)
+                $outBoard .= $gameLogic->getBoard()[$i];
 
-        // check game end
-        $gameResult = $gameLogic->checkGameEnd();
-        if ($gameResult != GameLogic::NOTEND)
-        {
-            // update game result
-            $game->result = Self::GameResultNames[$gameResult];
-            $game->end_at = $currentTime;
-        }
+            // update last beat
+            if ($lastBeat !== NULL)
+            {
+                $game->last_start = $lastBeat[0];
+                $game->last_beat = $lastBeat[1];
+            }
+            else
+            {
+                // if null
+                $game->last_start = NULL;
+                $game->last_beat = NULL;
+            }
+            // update current isplayer1move
+            $game->player1_move = $gameLogic->isPlayer1MakeMove();
 
-        $game->moves()->save($move);
+            // check game end
+            $gameResult = $gameLogic->checkGameEnd();
+            if ($gameResult != GameLogic::NOTEND)
+            {
+                // update game result
+                $game->result = Self::GameResultNames[$gameResult];
+                $game->end_at = $currentTime;
+            }
 
-        return [ 'player1Move' => $gameLogic->isPlayer1MakeMove() ];
+            $game->moves()->save($move);
+        });
+
+        if ($error !== NULL)
+            return [ 'error' => $error ];
+        return [ 'player1Move' => $outIsPlayer1Move ];
     }
 }
