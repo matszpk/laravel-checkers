1c2c8cab7ca3361c92a1c2f09336defeabc6949f
matszpk <matszpk@interia.pl>
2018-11-13 09:14:26 +0100

    Database migrations (change and revert).

---
+++ database/migrations/2018_11_13_080848_single_comments.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SingleComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::drop('gcomments');
+        Schema::drop('ucomments');
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('commentable');
+            $table->text('content');
+            $table->unsignedBigInteger('likes')->default(0);
+            $table->timestamps();
+            $table->unsignedBigInteger('writer_id');
+            $table->foreign('writer_id')->references('id')->on('users')->
+                    onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('comments');
+        Schema::create('ucomments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('commentable_id');
+            $table->foreign('commentable_id')->references('id')->on('users')->
+                    onDelete('cascade');
+            $table->text('content');
+            $table->unsignedBigInteger('likes')->default(0);
+            $table->timestamps();
+            $table->unsignedBigInteger('writer_id');
+            $table->foreign('writer_id')->references('id')->on('users')->
+                    onDelete('cascade');
+        });
+        Schema::create('gcomments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('commentable_id');
+            $table->foreign('commentable_id')->references('id')->on('games')->
+                    onDelete('cascade');
+            $table->text('content');
+            $table->unsignedBigInteger('likes')->default(0);
+            $table->timestamps();
+            $table->unsignedBigInteger('writer_id');
+            $table->foreign('writer_id')->references('id')->on('users')->
+                    onDelete('cascade');
+        });
+    }
+}
