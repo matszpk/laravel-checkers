c43753a514fc7320ef8b6869961c6068fc0fdeea
matszpk <matszpk@interia.pl>
2018-11-14 08:45:24 +0100

    Parametrize SASS (for game board). Remove element styles in play view template.

--- resources/views/game/play.blade.php
+++ resources/views/game/play.blade.php
@@ -11,45 +11,42 @@
 @section('script')
 @endsection
 
+@php($boardDim = \App\Logic\GameLogic::BOARDDIM)
+
 @section('main')
     <div id='checkers_board'>
         <div id='checkers_board_main'>
-        @for ($i = 9; $i >= 0; $i--)
-            <div class='checkers_board_row' id="checkers_board_row{{$i}}"
-                style="top: {{(9-$i)*40 }}px;">
-            @for ($j = 0; $j < 10; $j++)
+        @for ($i = 0; $i < $boardDim; $i++)
+            <div class='checkers_board_row' id="checkers_board_row{{$i}}">
+            @for ($j = 0; $j < $boardDim; $j++)
                 <div class="checkers_board_cell
+                        checkers_board_cell{{$j}}_in_row
                         checkers_board_cell_{{ ($i&1)^($j&1) ? 'white' : 'black' }}"
-                        id="checkers_board_cell{{$i}}{{$j}}"
-                    style="left: {{ ($j)*40 }}px;">
+                        id="checkers_board_cell{{$i}}{{$j}}">
                 </div>
             @endfor
             </div>
         @endfor
         </div>
         <div id='checkers_yaxis_left' class='checkers_axis'>
-            @for ($i = 9; $i >= 0; $i--)
-            <div class='checkers_yaxis_ypos'
-                style="top: {{ (9-$i)*40 }}px;">{{ $i+1 }}</div>
+            @for ($i = 0; $i < $boardDim; $i++)
+            <div class='checkers_yaxis_ypos checkers_yaxis_ypos{{($i)}}'>{{ $i+1 }}</div>
             @endfor
         </div>
         <div id='checkers_yaxis_right' class='checkers_axis'>
-            @for ($i = 9; $i >= 0; $i--)
-            <div class='checkers_yaxis_ypos'
-                style="top: {{ (9-$i)*40 }}px;">{{ $i+1 }}</div>
+            @for ($i = 0; $i < $boardDim; $i++)
+            <div class='checkers_yaxis_ypos checkers_yaxis_ypos{{$i}}'>{{ $i+1 }}</div>
             @endfor
         </div>
 
         <div id='checkers_xaxis_top' class='checkers_axis'>
-            @for ($i = 0; $i < 10; $i++)
-            <div class='checkers_xaxis_xpos'
-                style="left: {{ $i*40 }}px;">{{ chr(65+$i) }}</div>
+            @for ($i = 0; $i < $boardDim; $i++)
+            <div class='checkers_xaxis_xpos checkers_xaxis_xpos{{$i}}'>{{ chr(65+$i) }}</div>
             @endfor
         </div>
         <div id='checkers_xaxis_bottom' class='checkers_axis'>
-            @for ($i = 0; $i < 10; $i++)
-            <div class='checkers_xaxis_xpos'
-                style="left: {{ $i*40 }}px;">{{ chr(65+$i) }}</div>
+            @for ($i = 0; $i < $boardDim; $i++)
+            <div class='checkers_xaxis_xpos checkers_xaxis_xpos{{$i}}'>{{ chr(65+$i) }}</div>
             @endfor
         </div>
     </div>
