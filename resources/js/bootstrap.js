1fce113676a8fc88bdf88af34e23e3bf7757eba2
matszpk <matszpk@interia.pl>
2018-11-20 19:52:07 +0100

    Add core-js for IE11.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+require('core-js');
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
