f629a8c6b1d5228a49150435dbb1ee16299142da
matszpk <matszpk@interia.pl>
2018-11-25 20:55:39 +0100

    Add to fillable in User missing 'role' column.

--- app/User.php
+++ app/User.php
@@ -2,9 +2,7 @@
 
 namespace App;
 
-use Illuminate\Support\Facades\App;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
@@ -17,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role',
     ];
 
     /**
